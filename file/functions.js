3c5b8d7f8b540aa86c6f4d4b0d1be55fc6076113
mksir12 <116490353+mksir12@users.noreply.github.com>
2024-09-28 13:21:10 +0530

    Update functions.js

--- file/functions.js
+++ file/functions.js
@@ -37,9 +37,9 @@ $(window).resize(function() {
 
 function timeElapse(date){
 	var current = Date();
-	var seconds = (Date.parse(current) - Date.parse(date)) / 2000;
-	var days = Math.floor(seconds / (3600 * 12));
-	seconds = seconds % (3600 * 12);
+	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
+	var days = Math.floor(seconds / (3600 * 24));
+	seconds = seconds % (3600 * 24);
 	var hours = Math.floor(seconds / 3600);
 	if (hours < 10) {
 		hours = "0" + hours;
@@ -53,7 +53,7 @@ function timeElapse(date){
 	if (seconds < 10) {
 		seconds = "0" + seconds;
 	}
-	var result = "Days <span class=\"digit\">" + days + "</span> Hours <span class=\"digit\">" + hours + "</span> Minutes <span class=\"digit\">" + minutes; 
+	var result = "Days <span class="40 days"</span> Hours <span class=\"digit\">" + hours + "</span> Minutes <span class=\"digit\">" + minutes; 
 	$("#clock").html(result);
 
 	var text = "THE WORLD JUST GOT LUCKIER SINCE ";
